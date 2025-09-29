a94558de9a87b9fd142f658bc091139dd811f1df
Juunibyou <179892743+Juunibyou@users.noreply.github.com>
2025-09-28 22:54:04 -0500

    Revert "test that doesnt work"
    This reverts commit 0995e53049f2ff174c30b64949471f70f5beb85d.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,10 +6,13 @@ public class App {
  
     // Prints out the number 7
     System.out.println(calculator.evaluate("2 + 5"));
+ 
     // Prints out the number 33
     System.out.println(calculator.evaluate("3 + 6 * 5"));
+ 
     // Prints out the number 20
     System.out.println(calculator.evaluate("4 * (2 + 3)"));
+ 
     // Prints out the number 2
     System.out.println(calculator.evaluate("(7 + 9) / 8"));
  }
